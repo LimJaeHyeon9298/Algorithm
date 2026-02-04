4f20949807d0752a9e1f015a637191e3ae6bf5bc
LimJaeHyeon9298 <115773990+LimJaeHyeon9298@users.noreply.github.com>
2026-02-04 11:59:55 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ n_str:String) -> String {
+    var result = n_str
+    for i in n_str {
+        
+        
+        if i != "0" { 
+                     break}
+        result = String(result.dropFirst())
+    }
+    return result 
+}
