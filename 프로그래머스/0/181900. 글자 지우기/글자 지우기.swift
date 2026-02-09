3069b498c01f06ec6420238189defd40c0a0e0e0
LimJaeHyeon9298 <115773990+LimJaeHyeon9298@users.noreply.github.com>
2026-02-09 23:58:58 +0900

    [level 0] Title: 글자 지우기, Time: 0.21 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.swift
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.swift
@@ -1,9 +1,5 @@
 import Foundation
 
-func solution(_ my_string: String, _ indices: [Int]) -> String {
-    let indexSet = Set(indices)  
-    
-    return String(my_string.enumerated().filter {
-        !indexSet.contains($0.offset)
-    }.map(\.element))
+func solution(_ my_string:String, _ indices:[Int]) -> String {
+    return ""
 }
